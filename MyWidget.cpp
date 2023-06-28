dc0833460bc9df2f465445fcb7df5b321624160c
j-hap <ju.hapke@gmail.com>
2023-06-28 08:05:35 +0200

    added lambda return type, so gcc does not complain about conversion to std::function

--- MyWidget.cpp
+++ MyWidget.cpp
@@ -8,15 +8,11 @@ MyWidget::MyWidget() {
   lineEdit_ = new QLineEdit(this);
 #ifdef ENABLE_BUG
   connect(lineEdit_, &QLineEdit::textChanged, this, [this](QString const &s) {
-    emit propertyChanged(s, [](MyStruct const &s) {
-      return s.prop1;
-    });
+    emit propertyChanged(s, [](MyStruct const &s) -> std::string const & { return s.prop1; });
   });
 #else
   connect(lineEdit_, &QLineEdit::textChanged, this, [this](QString const &s) {
-    emit propertyChanged(s, [](MyStruct &s) {
-      return s.prop1;
-    });
+    emit propertyChanged(s, [](MyStruct &s) -> std::string const & { return s.prop1; });
   });
 #endif
 }
